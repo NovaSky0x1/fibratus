8479234e6f5d3844bbf120090acefa25e25fdd55
Joshua Strickland <jstrickland@secnap.com>
2026-04-04 19:31:43 -0400

    fix(dashboard): Remove unused queryClient in Admin top-level

--- web/dashboard/src/pages/Admin.tsx
+++ web/dashboard/src/pages/Admin.tsx
@@ -21,7 +21,6 @@ const passwordRules = [
 ]
 
 export default function Admin() {
-  const _queryClient = useQueryClient()
   const [tab, setTab] = useState<Tab>('accounts')
   const [filterAccountId, setFilterAccountId] = useState<string | null>(null)
 
